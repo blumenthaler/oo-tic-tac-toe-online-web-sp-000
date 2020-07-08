145d6e8d39ded3a669413db4610762d520fd30b1
Alex Blumenthaler <blumenthaleralex@gmail.com>
2020-07-08 17:54:21 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -102,4 +102,15 @@ class TicTacToe
     end
   end
   
+  def play(board)
+    until over?(board) 
+      turn(board)
+    end 
+    if won?(board)
+      winner(board) == "X" || winner(board) == "O" 
+      puts "Congratulations #{winner(board)}!" 
+    elsif draw?(board)
+      puts "Cat's Game!" 
+    end 
+  
 end
